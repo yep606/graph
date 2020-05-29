0ad9f1beb72e5ee5e4d9c86e42e893d8704bf89a
yep606 <ilya.rogov.vladislav@gmail.com>
2020-05-29 20:54:31 +0300

    methods

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,7 +1,31 @@
 package base;
 
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        DirectedGraph graph = new DirectedGraph();
+//        graph.addVertex(1);
+//        graph.addVertex(2);
+//        graph.addEdge(1,2);
+//        graph.addVertex(3);
+//
+//
+//        graph.addEdge(1,3);
+//
+//        graph.display();
+//
+//        graph.saveGraph();
+
+        System.out.println("Loading graph");
+        graph.loadGraph();
+
+        graph.display();
+
+        char c = '\n';
+        System.out.print((int) c);
 
     }
 }
