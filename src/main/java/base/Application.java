e94ed939de800518630c7db07ef4c7b109b12e68
khrnt <tanya51rus@gmail.com>
2020-05-29 18:24:02 +0300

    Initial commit

---
+++ src/main/java/base/Application.java
@@ -0,0 +1,7 @@
+package base;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/base/DirectedGraph.java
@@ -0,0 +1,30 @@
+package base;
+
+import java.util.LinkedList;
+
+public class DirectedGraph {
+
+    private LinkedList<Integer>[] adj = new LinkedList[100];
+
+    public void addVertex(int vertex){}
+
+    public void addEdge(int vertex1, int vertex2){}
+
+    public void delVertex(int vertex){}
+
+    public void delEdge(int vertex1, int vertex2){}
+
+    public void loadGraph(){}
+
+    public void saveGraph(){}
+
+    // 5 task option
+
+    public void p(DirectedGraph graph){}
+
+    public void dMinus(LinkedList<Integer> vertex){}
+
+    public void D(int v, int n){}
+
+
+}
