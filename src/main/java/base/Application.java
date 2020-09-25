801b8f1dcf8fa0093e6d140ffcce87dd8e271de4
yep606 <ilya.rogov.vladislav@gmail.com>
2020-09-25 21:38:09 +0300

    Init SU_AI project

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,28 +1,13 @@
 package base;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args){
 
-        Stack<Integer> stack = new Stack<>(10);
-        System.out.println("Проверка на пустоту:");
-        System.out.println(stack.empty());
-        System.out.println("--------------");
-
-        System.out.println("Добавление элементов: 1,2,3,4");
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        System.out.println("--------------");
-
-        System.out.println("Вывод элементов в порядке расположения");
-        stack.printStack();
-        System.out.println("--------------");
-
-        System.out.println("Удаление элемента: ");
-        stack.pop();
-        stack.printStack();
-        System.out.println("--------------");
-
+    public static void main(String[] args) {
+       SpringApplication.run(Application.class, args);
     }
+
 }
